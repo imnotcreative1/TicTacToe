5708e8db829ff4184478d8d18736e520efd08d60
Matt <12mjs17@queensu.ca>
2018-03-26 21:01:39 -0400

    added redux and react-router-dom and a login page, all WIP

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,22 +1,35 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import { Card, TextField } from 'material-ui';
+import './Login.css';
+
+const containerStyle: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  height: '100%',
+};
+
+const style: React.CSSProperties = {
+  height: '300px',
+  width: '300px',
+};
 
 class Login extends React.Component {
   render() {
     return (
-      <div className="login-container">
-        <div className="header">
-          Login
-        </div>
-        <div className="username">
-          Enter you username
-        </div>
-        <div className="password">
-          Enter you password
-        </div>
-        <Link to="/app">
-          Sign in
-        </Link>
+      <div className="login-page-container">
+        <Card containerStyle={containerStyle} style={style}>
+          <div className="header">
+            Log In
+          </div>
+          <TextField hintText="Username"/>
+          <TextField hintText="Password"/>
+          <Link to="/app">
+            Sign in
+          </Link>
+        </Card>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import configureStore from './configureStore';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './components/Login';
 import { Switch } from 'react-router';
+import { MuiThemeProvider } from 'material-ui/styles';
 
 // TODO: remove unnecessary node modules i.e. react-router-redux
 
@@ -15,14 +16,16 @@ const store = configureStore({});
 
 // TODO: make the app a protected path and figure out routing
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route path="/" component={Login} exact={true}/>
-        <Route path="/app" component={App}/>
-      </Switch>
-    </Router>
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/" component={Login} exact={true}/>
+          <Route path="/app" component={App}/>
+        </Switch>
+      </Router>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
